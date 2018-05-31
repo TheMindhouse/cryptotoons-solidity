1caeaf6d425aa2226be446220e55c6eb73113551
kmisztal <krmisztal@gmail.com>
2018-05-31 13:39:33 +0200

    Changed compiler version.

--- contracts/ToonMinting.sol
+++ contracts/ToonMinting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 import "./ToonBase.sol";
 import "./lib/SafeMath32.sol";
--- contracts/ToonsAggregator.sol
+++ contracts/ToonsAggregator.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.24;
 
 import "./lib/AddressUtils.sol";
 import "./ToonInterface.sol";
