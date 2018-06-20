9f00dc5db0604efd2ceb6b9e0226ccd626fa5591
kmisztal <krmisztal@gmail.com>
2018-06-20 15:50:35 +0200

    Configured migrations

---
+++ migrations/2_contracts.js
@@ -0,0 +1,10 @@
+const SaleAuction = artifacts.require("./ClockAuction.sol");
+const CryptoToon = artifacts.require("./CryptoToon.sol");
+
+module.exports = function (deployer) {
+    deployer.deploy(SaleAuction, 500, 5000);
+    deployer.deploy(CryptoToon, "toon", "CTN", 100, 10, "0x0")
+        .then(instance => {
+            instance.setSaleAuctionAddress(SaleAuction.address);
+        })
+};
--- truffle.js
+++ truffle.js
@@ -1,4 +1,21 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+    networks: {
+        dev: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*", // Match any network id
+            gas: 7000000
+        },
+    },
+    mocha: {
+        useColors: true
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
 };
